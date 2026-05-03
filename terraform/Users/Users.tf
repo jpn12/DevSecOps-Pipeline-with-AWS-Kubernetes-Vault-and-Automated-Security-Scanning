10310a71ce658d379bfcb2711c0531ed98767936
João paulo <111157391+jpn12@users.noreply.github.com>
2026-05-03 19:02:19 -0300

    update

--- terraform/Users/Users.tf
+++ terraform/Users/Users.tf
@@ -1,42 +1,5 @@
 
-data "aws_caller_identity" "current" {}
-
-############## Roles ################
-resource "aws_iam_role" "developer_role" {
-  name = "developer_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-
 resource "aws_iam_group" "developers" {
   name = "developers"
   path = "/users/"
-}
-
-resource "aws_iam_group_policy" "developer_role" {
-  name  = "developer_role"
-  group = aws_iam_group.developers.name
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = "sts:AssumeRole"
-        Resource = aws_iam_role.developer_role.arn
-      }
-    ]
-  })
 }
